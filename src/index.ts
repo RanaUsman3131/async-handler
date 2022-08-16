5dd52c7f55df0b72520c330b1f9f86e0d46016b8
Usman <usmankhan.vingo3tier@nxvt.com>
2022-08-16 17:29:49 +0500

    Ci/Ci

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { catchAsync } from './catchAsync';
+export { errorConverter, errorHandler } from './handler';
 
 export const asyncHandle = function(fn: Function) {
   Promise.resolve(fn(arguments)).catch(() => {
